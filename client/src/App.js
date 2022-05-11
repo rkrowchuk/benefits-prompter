412d5ce3d80b2669536da639e91d27b4083beeee
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-11 14:24:30 -0600

    add catch

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import {React, useEffect, useState} from "react";
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
 
-function App() {
+export default function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data.message))
+      .catch((err) => console.log("error:", err));
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
 }
-
-export default App;
