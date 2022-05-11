8267a23a1ec0b58d14d53c7887133d9913966e8f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-11 13:55:17 -0600

    comment out code; delete files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
+// const express = require("express");
+// const router = express.Router();
+// const mongoose = require("mongoose");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+// /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
 
-const TestModel = require("../models/TestDB");
+// const TestModel = require("../models/TestDB");
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://rkrowchuk:BenefitsPassword@benefitsdb.qdooj.mongodb.net/testDB?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-  }
-);
-/* GET home page. */
-app.get("/", async (req, res) => {
-  const user = new TestModel({
-    userName: "Apricot",
-    userEmail: "apricot@fake.com",
-    userBirthday: "1990-01-05",
-  });
-  console.log("made it here");
-  try {
-    await user.save();
-  } catch (err) {
-    console.log("error:", err);
-  }
-});
+// mongoose.connect(
+//   "mongodb+srv://rkrowchuk:BenefitsPassword@benefitsdb.qdooj.mongodb.net/testDB?retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//   }
+// );
+// /* GET home page. */
+// app.get("/", async (req, res) => {
+//   const user = new TestModel({
+//     userName: "Apricot",
+//     userEmail: "apricot@fake.com",
+//     userBirthday: "1990-01-05",
+//   });
+//   console.log("made it here");
+//   try {
+//     await user.save();
+//   } catch (err) {
+//     console.log("error:", err);
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
--- server/routes/testAPI.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-/* GET test page. */
-router.get("/", function (req, res, next) {
-  res.send("API is working properly");
-});
-
-module.exports = router;
