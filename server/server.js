c2641dfc7ac67d7c87f3b50abd8228905a9fda5f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-25 12:26:04 -0600

    delete file and clean up

--- server/server.js
+++ server/server.js
@@ -16,27 +16,23 @@ app.listen(9000, function () {
   console.log("Listening on 9000");
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello, world!");
-// });
-
 const TestModel = require("./models/TestDB");
 
-
 app.get("/", (req, res) => {
-    const user = new TestModel({
-      userName: "Apricot",
-      userEmail: "apricot@fake.com",
-      userBirthday: "1990-01-05",
-    });
-    user.save()
+  const user = new TestModel({
+    userName: "Frosty",
+    userEmail: "frosty@fake.com",
+    userBirthday: "2021-11-12",
+  });
+  user
+    .save()
     .then(() => {
       console.log("made it here with user", user);
-    }) 
+      res.send("Hello, world!");
+    })
     .catch((err) => {
       console.log("error:", err);
-    })
+    });
 });
-  
-  module.exports = router;
 
+module.exports = router;
