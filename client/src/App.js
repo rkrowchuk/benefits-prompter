89e4260967df46700433b2542288504efc4a33e7
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-25 13:56:50 -0600

    comment out useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import Register from "./components/Register";
 export default function App() {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-      .catch((err) => console.log("error:", err));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message))
+  //     .catch((err) => console.log("error:", err));
+  // }, []);
 
   return (
     <div className="App">
