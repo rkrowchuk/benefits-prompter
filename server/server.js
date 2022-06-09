af77cbdde304368ac15b490719840e8970a9fcf2
Kelsi Olstad <kelsiolstad@gmail.com>
2022-06-09 16:24:20 -0600

    update 'new' route to 'register'; user authentication - check for email and password in db

--- server/server.js
+++ server/server.js
@@ -37,20 +37,38 @@ app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
-app.get("/new", (req, res) => {
+app.get("/register", (req, res) => {
   res.send("New User!");
 });
 
-app.post("/new", (req, res) => {
+app.post("/register", (req, res) => {
   const hashedPass = bcrypt.hashSync(req.body.password, saltRounds);
   const user = new TestModel({
     userName: req.body.name,
     userEmail: req.body.email,
     userBirthday: req.body.birthdate,
-    userPassword: hashedPass
+    userPassword: hashedPass,
   });
   user.save().catch((err) => {
     console.log("error:", err);
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.send("Login");
+});
+
+app.post("/login", (req, res) => {
+  console.log("req", req.body.password);
+  TestModel.findOne({ userEmail: req.body.email }, (err, success) => {
+    if (
+      !success ||
+      !bcrypt.compareSync(req.body.password, success.userPassword)
+    ) {
+      return console.log("Password/email cannot be found");
+    } else {
+      console.log("Successful login!");
+    }
   });
 });
 
