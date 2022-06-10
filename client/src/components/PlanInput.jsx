46406173246aa8f121f48ed9626bd829b1eb0e69
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-06-10 15:52:57 -0700

    add list of suggested plan categories to plan input form

--- client/src/components/PlanInput.jsx
+++ client/src/components/PlanInput.jsx
@@ -6,8 +6,30 @@ export default function PlanInput() {
       Plan Input
       <form>
         <label>
-          Renewal Date:<input></input>
+          Renewal Date:<input type="date"></input>
         </label>
+        <label>
+          Category:
+        </label>
+        <select>
+          <option value="dental">Dental</option>
+          <option value="massage">Massage</option>
+          <option value="physio">Physiotherapy</option>
+          <option value="vision">Vision</option>
+          <option value="prescription">Prescription</option>
+          <option value="health-spending">Health Spending</option>
+          <option value="orthotics">Orthotics</option>
+          <option value="orthodontics">Orthodontics</option>
+          <option value="ambulance">Ambulance</option>
+          <option value="clinic-visit">Clinic Visit</option>
+          <option value="counselling">Counselling</option>
+          <option value="psychologist">Psychologist</option>
+          <option value="vaccinations">Vaccinations</option>
+          <option value="travel-insurance">Travel Insurance</option>
+          <option value="fertility">Fertility</option>
+          <option value="disability">Disability</option>
+          <option value="other">Other</option>
+        </select>
       </form>
     </div>
   );
