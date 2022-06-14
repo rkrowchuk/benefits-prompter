7b3ced084ec322f76d501142afdfd810e35306c8
Kelsi Olstad <kelsiolstad@gmail.com>
2022-06-14 17:04:32 -0600

    alphabetize categories; add goals as comments

--- client/src/components/PlanInput.jsx
+++ client/src/components/PlanInput.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Form from "./Category";
 
 export default function PlanInput() {
+  //App.js is parent component
   // const [inputFields, setInputFields] = useState([ { RenewalDate: "", Category: ""}])
+
+  //Goals:
+  // 1. Render each chosen category in a list within the <form>
+
+  // 2. Update the state and send the list of categories to the database (associate with user as well)
+
   return (
     <div>
       Plan Input
@@ -10,32 +18,33 @@ export default function PlanInput() {
         <label>
           Renewal Date:<input type="date"></input>
         </label>
-        <label>
-          Category:
-        </label>
+        <label>Category:</label>
         <select>
-          <option value="dental">Dental</option>
-          <option value="massage">Massage</option>
-          <option value="physio">Physiotherapy</option>
-          <option value="vision">Vision</option>
-          <option value="prescription">Prescription</option>
-          <option value="health-spending">Health Spending</option>
-          <option value="orthotics">Orthotics</option>
-          <option value="orthodontics">Orthodontics</option>
           <option value="ambulance">Ambulance</option>
           <option value="clinic-visit">Clinic Visit</option>
           <option value="counselling">Counselling</option>
-          <option value="psychologist">Psychologist</option>
-          <option value="vaccinations">Vaccinations</option>
-          <option value="travel-insurance">Travel Insurance</option>
-          <option value="fertility">Fertility</option>
+          <option value="dental">Dental</option>
           <option value="disability">Disability</option>
-          <option value="other">Other</option>
+          <option value="fertility">Fertility</option>
+          <option value="health-spending">Health Spending</option>
+          <option value="massage">Massage</option>
+          <option value="orthodontics">Orthodontics</option>
+          <option value="orthotics">Orthotics</option>
+
+          <option value="physio">Physiotherapy</option>
+          <option value="prescription">Prescription</option>
+          <option value="psychologist">Psychologist</option>
+          <option value="travel-insurance">Travel Insurance</option>
+          <option value="vaccinations">Vaccinations</option>
+          <option value="vision">Vision</option>
+
+          {/* <option value="other">Other</option> */}
         </select>
-        <label>Add a category</label>
-        <input type="text"></input>
+        {/* <label>Add a category</label>
+    <input type="text"></input> */}
       </form>
-      <button>Submit</button>
+      <button>Add Category</button>
+      <button>Submit Plan</button>
     </div>
   );
 }
