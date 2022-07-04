407d704ec7f7aca1e0e710f8c3cbb478818f04e5
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-07-04 16:25:52 -0700

    add links to navbar for dashboard and logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 // parent component is App
 
 export default function Navbar() {
 
   return (
-<div>Navbar</div>
+    <nav>
+      <ul>
+        <li><Link to="/dashboard" className="nav-link">Dashboard</Link></li>
+        <li><Link to="/" className="nav-link">Logout</Link></li>
+      </ul>
+    </nav>
   );
 }
